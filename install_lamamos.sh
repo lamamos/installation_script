65d56e6ca8d04b08a323873e3cf80f26b961184d
root <root@server.(none)>
2013-10-20 20:00:11 +0200

    Added the installation of Rex to the script

--- install_lamamos.sh
+++ install_lamamos.sh
@@ -1,1 +1,7 @@
-echo "kikooo"
+echo "=====We are going to install lamamos====="
+
+echo "===First we install rex==="
+echo 'deb http://rex.linux-files.org/debian/ wheezy rex' >> /etc/apt/sources.list
+wget -O - http://rex.linux-files.org/DPKG-GPG-KEY-REXIFY-REPO | apt-key add -
+apt-get update
+apt-get install -y rex
