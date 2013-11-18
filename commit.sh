50f6cb3293cac857bd67639dfbccc5adbe4ae9a0
karlito139 <karlito@martobre.fr>
2013-11-18 14:24:03 +0100

    Added a log.pm file which contain the function to read the config file.

--- commit.sh
+++ commit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "kikooo"
 rm -r lamamos
 mkdir lamamos
 
 cp /etc/lamamos/lamamos.conf lamamos/
 cp -r /etc/lamamos/rex lamamos/
+
+echo "done"
---
+++ lamamos/rex/lib/log.pm
@@ -0,0 +1,26 @@
+sub ReadCfg
+{
+    my $file = $_[0];
+
+    our $err;
+
+    {   # Put config data into a separate namespace
+        package CFG;
+
+        # Process the contents of the config file
+        my $rc = do($file);
+
+        # Check for errors
+        if ($@) {
+            $::err = "ERROR: Failure compiling '$file' - $@";
+        } elsif (! defined($rc)) {
+            $::err = "ERROR: Failure reading '$file' - $!";
+        } elsif (! $rc) {
+            $::err = "ERROR: Failure processing '$file'";
+        }
+    }
+
+    return ($err);
+}
+
+1;
