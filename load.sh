94dadb963ec14d11055104af732d2a5809c7c594
karlito139 <karlito@martobre.fr>
2013-12-08 14:00:14 +0100

    Added the install of the authkey.

--- load.sh
+++ load.sh
@@ -5,6 +5,7 @@ mkdir /etc/lamamos/rex
 rm /etc/lamamos/lamamos.conf
 
 cp lamamos/lamamos.conf /etc/lamamos/lamamos.conf
+cp lamamos/authkey /etc/lamamos/authkey
 cp -r lamamos/rex /etc/lamamos/
 
 echo "done"
