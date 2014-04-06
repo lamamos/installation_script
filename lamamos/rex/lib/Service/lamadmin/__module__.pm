aee2ba7f31991236b2d57cb3cab22ecb06441ed1
karlito139 <karlito@martobre.fr>
2014-04-06 16:11:10 +0200

    Correct small bug in the copie of lamadmin

--- lamamos/rex/lib/Service/lamadmin/__module__.pm
+++ lamamos/rex/lib/Service/lamadmin/__module__.pm
@@ -14,7 +14,7 @@ task define => sub {
     group => "www-data",
     mode  => 755;
 
-  `cp -r files/lamadmin $variables->{install_path}`;
+  `cp -r files/lamadmin/* $variables->{install_path}`;
 };
 
 1;
