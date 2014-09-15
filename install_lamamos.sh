7b84406b34e49b00acf1e7e26753e994b9e0b2ae
karlito139 <karlito@martobre.fr>
2014-09-15 09:54:10 +0200

    added a test if we are root at the begining of the install

--- install_lamamos.sh
+++ install_lamamos.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+#test if we are root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+fi
+
+
 echo -en "\ec"
 echo "=====We are going to install git====="
 apt-get update
